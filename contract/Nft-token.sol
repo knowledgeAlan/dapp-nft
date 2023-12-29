308288f7701d669acdb35fca0e8a085ac8bbee2d
support@uniwebpay.com <support@uniwebpay.com>
2023-12-29 10:42:47 +0800

    add ownable and event

--- contract/Nft-token.sol
+++ contract/Nft-token.sol
@@ -3,7 +3,10 @@
 pragma solidity >= 0.8.18;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract ZcdNftToken is ERC721URIStorage,Ownable {
+contract ZcdNftToken is ERC721URIStorage,Ownable(msg.sender) {
+
+    event mintTokenEvent(address sender,uint256 tokenId,string uri);
+
     constructor() ERC721("Zcd grasshopper","ZGH") {
         
     }
@@ -14,6 +17,8 @@ contract ZcdNftToken is ERC721URIStorage,Ownable {
         _mint(_to,_tokenId);
         _setTokenURI(_tokenId,_uri);
         
+        
+        emit mintTokenEvent(_to,_tokenId,_uri);    
     }
 
 }
